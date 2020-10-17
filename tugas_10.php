f03d4054937ae8c9a69cb22fca9ec66c0a275359
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-18 00:39:42 +0700

    Add files via upload

---
+++ tugas_10.php
@@ -0,0 +1,30 @@
+
+<?php
+$arr = array(1,3,8,2,5,7,4,0);
+
+print("Sebelum Di Sorting :<br>");
+print_r($arr);
+
+$arr = bubble_sort($arr);
+print("<br> Setelah Disorting (Buble Sort) :<br>");
+print_r($arr);
+
+
+function bubble_sort($arr) {
+	do
+	{
+		$tukar = false;
+		for( $i = 0; $i < count($arr)-1; $i++ )
+		{
+			if( $arr[$i] > $arr[$i + 1] )
+			{
+				list( $arr[$i + 1], $arr[$i] ) =
+						array( $arr[$i], $arr[$i + 1] );
+				$tukar = true;
+			}
+		}
+	}
+	while( $tukar );
+return $arr;
+}
+?>
